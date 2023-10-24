568199ea2b8969aff4fabd4a112869d65294bdc2
samfernandes1 <sam.fernandes@speedio.com.br>
2023-10-24 16:30:17 -0300

    rost alls

--- em_track_conf/settings.py
+++ em_track_conf/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-6udh1zw$byi)@8@t-ff$)f_-&zsp49xxkpy3uxgnwpdp+zg10x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
